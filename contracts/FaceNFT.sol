6ee9411f57530587efdb3085272fb602e2190be2
Bryan Tan <bryan.tan@globaltium.com>
2022-04-15 13:47:14 +0800

    clean up and update nft contract

--- contracts/FaceNFT.sol
+++ contracts/FaceNFT.sol
@@ -3,18 +3,22 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract FaceNFT is ERC721, ERC721URIStorage, Ownable {
+contract FaceNFT is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
-    uint256 _mintCost = 1;
-    mapping(string => uint8) existingURIs;
+    uint256 private _mintCost = 1;
+    uint256 private _maxSupply = 10;
+    mapping(string => uint8) _existingURIs;
+    mapping(uint256 => bool) _deletedTokens;
 
     modifier isMintable(string memory uri) {
-        require(existingURIs[uri] != 1, "NFT already minted!");
+        require(_existingURIs[uri] != 1, "NFT already minted!");
+        require(totalSupply() < _maxSupply, "Total supply reached!");
         _;
     }
 
@@ -31,15 +35,56 @@ contract FaceNFT is ERC721, ERC721URIStorage, Ownable {
 
     function safeMint(address to, string memory uri)
         public
-        onlyOwner
+        payable
         isMintable(uri)
+        hasPaymentAmount
+        returns (uint256)
     {
-        _tokenIdCounter.increment();
-
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
+        _existingURIs[uri] = 1;
+
+        return tokenId;
+    }
+
+    function isContentOwned(string memory uri) public view returns (bool) {
+        return _existingURIs[uri] == 1;
+    }
+
+    function isContentOwner(uint256 tokenId) public view returns (bool) {
+        return ownerOf(tokenId) == msg.sender;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _tokenIdCounter.current();
+    }
+
+    function setMintCost(uint256 amount) public onlyOwner {
+        _mintCost = amount;
+    }
+
+    function mintCost() public view returns (uint256) {
+        return _mintCost;
+    }
+
+    function setMaxSupply(uint256 amount) public onlyOwner {
+        _maxSupply = amount;
+    }
+
+    function maxSupply() public view returns (uint256) {
+        return _maxSupply;
+    }
+
+    function isTokenDeleted(uint256 tokenId) public view returns (bool) {
+        return _deletedTokens[tokenId] == true;
+    }
+
+    // Custom overrides
+    function burn(uint256 tokenId) public virtual override(ERC721Burnable) {
+        super.burn(tokenId);
+        _deletedTokens[tokenId] = true;
     }
 
     // The following functions are overrides required by Solidity.
@@ -58,42 +103,5 @@ contract FaceNFT is ERC721, ERC721URIStorage, Ownable {
         returns (string memory)
     {
         return super.tokenURI(tokenId);
-    }
-
-    function isContentOwned(string memory uri) public view returns (bool) {
-        return existingURIs[uri] == 1;
-    }
-
-    function isContentOwner(uint256 tokenId) public view returns (bool) {
-        return ownerOf(tokenId) == msg.sender;
-    }
-
-    function payToMint(address recipient, string memory uri)
-        public
-        payable
-        isMintable(uri)
-        hasPaymentAmount
-        returns (uint256)
-    {
-        uint256 newItemId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        existingURIs[uri] = 1;
-
-        _mint(recipient, newItemId);
-        _setTokenURI(newItemId, uri);
-
-        return newItemId;
-    }
-
-    function count() public view returns (uint256) {
-        return _tokenIdCounter.current();
-    }
-
-    function setMintCost(uint256 amount) public onlyOwner {
-        _mintCost = amount;
-    }
-
-    function mintCost() public view returns (uint256) {
-        return _mintCost;
     }
 }
