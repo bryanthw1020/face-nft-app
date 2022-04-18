1d6a26392544cbef382846f3553bfcd5ddfbdeae
Bryan Tan <bryan.tan@globaltium.com>
2022-04-18 10:14:49 +0800

    add ui handling if no wallet connected

--- client/components/CollectionList.vue
+++ client/components/CollectionList.vue
@@ -1,20 +1,27 @@
 <template>
   <v-card>
-    <v-card-title> All NFTs </v-card-title>
-    <v-card-subtitle>
-      Current Mint Cost: {{ currentMintCost }} ETH
-    </v-card-subtitle>
+    <template v-if="account">
+      <v-card-title> All NFTs </v-card-title>
+      <v-card-subtitle>
+        Current Mint Cost: {{ currentMintCost }} ETH
+      </v-card-subtitle>
 
-    <div class="px-4 py-6 text-center">
-      <v-row>
-        <v-col cols="12" sm="3" v-for="item in items" :key="item.id">
-          <collection-list-item
-            :token-id="item.tokenId"
-            :mint-cost="currentMintCost"
-          />
-        </v-col>
-      </v-row>
-    </div>
+      <div class="px-4 py-6 text-center">
+        <v-row>
+          <v-col cols="12" sm="3" v-for="item in items" :key="item.id">
+            <collection-list-item
+              :token-id="item.tokenId"
+              :mint-cost="currentMintCost"
+            />
+          </v-col>
+        </v-row>
+      </div>
+    </template>
+    <template v-else>
+      <v-card-text class="text-center py-10">
+        Please connect your wallet to proceed
+      </v-card-text>
+    </template>
   </v-card>
 </template>
 
